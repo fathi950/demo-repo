d53d4531b718994a4028429f9cd383464a0b5c42
N A <160292300+fathi950@users.noreply.github.com>
2024-03-06 17:11:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,31 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+    
+    def append(self,data):
+        node = Node(data,None)
+        if self.head is None:
+            self.head = node
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = node
+    
+    def print(self):
+        if self.head is None:
+            return
+        curr = self.head
+        while curr:
+            print(curr.data, end='->')
+            curr = curr.next
+        print('null')
 
 
 ll = LinkedList()
+
+ll.append(10)
+ll.append(20)
+ll.append(30)
+ll.append(40)
+ll.print()
